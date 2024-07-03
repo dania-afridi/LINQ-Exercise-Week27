a02dc7bcea22d19dcab4ffe0691bbf24ff79783d
dania-afridi <dania.awais33@gmail.com>
2024-07-03 09:43:30 +0200

    linq exercise added.

---
+++ LINQ-DB/Concert.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_DB
+{
+    public class Concert
+    {
+        public int Id { get; set; }
+        public bool ReducedVenue { get; set; }
+        public DateTime Date {  get; set; }
+        public string Performer { get; set; }
+        public int BeginsAt { get; set; }
+        public int FullCapacitySales { get; set; }
+        
+        public Concert(int id, bool reducedVenue, DateTime date, string performer, int beginsAt, int fullCapacitySales) {
+            Id = id;
+            ReducedVenue = reducedVenue;
+            Date = date;
+            Performer = performer;
+            BeginsAt = beginsAt;
+            FullCapacitySales = fullCapacitySales;
+        }
+    }
+}
---
+++ LINQ-DB/Queries.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace LINQ_DB
+{
+    public class Queries
+    { 
+        public static List<Concert> orderedBy_Date(List<Concert> concerts) {
+            return concerts 
+                .OrderBy(concert => concert.Date)
+                .ToList();
+        }
+
+        public static List<Concert> reducedVenue_Concerts(List<Concert> concerts)
+        {
+            return concerts
+                .Where(concert => concert.ReducedVenue = true)
+                .ToList();
+        }
+
+        public static List<Concert> concerts_2024(List<Concert> concerts)
+        {
+            return concerts
+                .Where(concert => concert.Date.Year == 2024)
+                .ToList();
+        }
+
+        public static List<Concert> biggest_ProtectedSales(List<Concert> concerts)
+        {
+            return concerts
+                .OrderByDescending(concert => concert.FullCapacitySales)
+                .Take(5)
+                .ToList();
+        }
+
+        public static List<Concert> friday_Concerts(List<Concert> concerts)
+        {
+            return concerts
+                .Where(concert => concert.Date.DayOfWeek == DayOfWeek.Friday)
+                .ToList();
+        }
+
+    }
+
+}
